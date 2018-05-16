df820dd0a2d24087d6d2c3d458b3cb952658280a
Bujamin <bujamin.saliu@gmail.com>
2018-05-16 22:30:12 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<h1>Intercom Testing</h1>
+				</header>
+				<p>Simple react app</p>
+			</div>
+		)
+	}
+}
+
+export default App
